df32687aa035f5edbda704690a4e9b7bca5a55a1
hediye853@gmail.com <hediye853@gmail.com>
2020-08-01 06:17:35 +0430

    Add Validate Files

---
+++ src/org/j2os/monitor/modules/common/model/service/MonitorLogService.java
@@ -0,0 +1,64 @@
+package org.j2os.monitor.modules.common.model.service;
+
+import org.j2os.monitor.modules.common.model.entity.MonitorLog;
+import org.j2os.monitor.modules.common.model.repository.MonitorLogRepository;
+import org.j2os.monitor.modules.utils.Interfaces.service.ServiceInterface;
+import org.j2os.monitor.modules.utils.MyBeanCopy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+@Service
+public class MonitorLogService implements ServiceInterface<MonitorLog> {
+    private MonitorLogRepository monitorLogRepository;
+
+    @Autowired
+    public MonitorLogService(MonitorLogRepository monitorLogRepository) {
+        this.monitorLogRepository = monitorLogRepository;
+    }
+
+    @Override
+    @Transactional
+    public void add(MonitorLog monitorLog) {
+        this.monitorLogRepository.save(monitorLog);
+    }
+
+    @Override
+    @Transactional
+    public void update(MonitorLog monitorLog) throws InvocationTargetException, IllegalAccessException {
+        MonitorLog exist = this.monitorLogRepository.findOne(MonitorLog.class,monitorLog.getMonitorlogId());
+        MyBeanCopy myBeanCopy = new MyBeanCopy();
+        myBeanCopy.copyProperties(exist, monitorLog);
+        this.monitorLogRepository.save(exist);
+    }
+
+    @Override
+    public List<MonitorLog> findAll() {
+        return this.monitorLogRepository.findAll(MonitorLog.class);
+    }
+
+    @Override
+    @Transactional
+    public void delete(MonitorLog monitorLog) {
+        this.monitorLogRepository.delete(monitorLog);
+    }
+
+    @Override
+    public MonitorLog findOne(MonitorLog monitorLog) {
+        return this.monitorLogRepository.findOne(MonitorLog.class,monitorLog.getMonitorlogId());
+    }
+
+    @Override
+    public Boolean existsById(long id) {
+        return this.monitorLogRepository.existsById(MonitorLog.class,id);
+    }
+
+    @Override
+    public MonitorLog findById(long id) {
+        return this.monitorLogRepository.findOne(MonitorLog.class,id);
+    }
+}
+
